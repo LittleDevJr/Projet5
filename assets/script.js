68b34fec5b7e08fedd79c96eb6ce54d0432ee90c
hollo <hollowpuf@hotmail.fr>
2024-07-20 21:02:06 +0200

    Projet terminé

--- assets/script.js
+++ assets/script.js
@@ -21,38 +21,6 @@ let flecheGauche = document.getElementById("left");
 let flecheDroite = document.getElementById("right");
 let currentSlideIndex = 0;
 
-
-/*
-function updateSlide() {
-    const slideImage = document.getElementById("banner-img");
-    const slideTagLine = document.getElementById("banner-tagline");
-
-    slideImage.src = slides[currentSlideIndex].image;
-    slideTagLine.innerHTML = slides[currentSlideIndex].tagLine;
-
-    // Réinitialise toutes les classes dot à dot
-    document.querySelectorAll('.dot').forEach(dot => dot.classList.remove('dot_selected'));
-
-    // Ajoute la classe dot_selected à la diapositive actuelle
-    document.querySelectorAll('.dot')[currentSlideIndex].classList.add('dot_selected');
-}
-
-flecheDroite.addEventListener("click", () => {
-    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-    updateSlide();
-});
-
-flecheGauche.addEventListener("click", () => {
-    currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
-    updateSlide();
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    updateSlide();
-});
-*/
-
-
 function deplacementIndex(){
 let image = document.getElementById("banner-img");
 let texte = document.getElementById("banner-tagline");
